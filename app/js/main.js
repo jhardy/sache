6bc110cb4aa454a570d9817215ac4826fd8ea614
Chris Michel <cm@chrismdesign.com>
2014-01-27 21:48:57 -0800

    fixed spacing in js file

--- app/js/main.js
+++ app/js/main.js
@@ -1,85 +1,83 @@
 $(function() {
 
-    $("#submit-form").submit(function(event) {
+  $("#submit-form").submit(function(event) {
 
-        event.preventDefault();
+    event.preventDefault();
 
-        var $form = $(this),
-            actionURL = $form.attr('action'),
-            formData = $form.serialize();
+    var $form = $(this),
+        actionURL = $form.attr('action'),
+        formData = $form.serialize();
 
-        console.log($form);
-        console.log(actionURL);
-        console.log(formData);
+    console.log($form);
+    console.log(actionURL);
+    console.log(formData);
 
-        $.ajax({
-            url: actionURL,
-            type: "POST",
-            data: formData,
-            beforeSend: function() {
-                $form.addClass('processing')
-            },
-            complete: function(jqXHR, status) {
-                console.log(jqXHR, status)
-                console.log(jqXHR.responseText);
-                $form.removeClass('processing')
-            },
-            success: function(e) { console.log("success: " + e)
+    $.ajax({
+        url: actionURL,
+        type: "POST",
+        data: formData,
+        beforeSend: function() {
+            $form.addClass('processing')
+        },
+        complete: function(jqXHR, status) {
+            console.log(jqXHR, status)
+            console.log(jqXHR.responseText);
+            $form.removeClass('processing')
+        },
+        success: function(e) { console.log("success: " + e)
 
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.log("jqXHR: ", jqXHR);
-                console.log("textStatus: ", textStatus);
-                console.log("errorThrown: ", errorThrown);
-                //console.log("error: " + textStatus.responseText)
-            }
-         });
-    });
-
-
-    var $addProject = $('[data-add-project]'),
-      $modalTarp = $('.modal-tarp'),
-      $projectModal = $('.add-project'),
-      $cancelProject = $('[data-cancel-project]'),
-      fadeSpeed = 200;
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log("jqXHR: ", jqXHR);
+            console.log("textStatus: ", textStatus);
+            console.log("errorThrown: ", errorThrown);
+            //console.log("error: " + textStatus.responseText)
+        }
+     });
+  });
 
 
-    var closeModal = function() {
-      $modalTarp.fadeOut(fadeSpeed);
-      $projectModal.removeClass('modal-open');
-    }
+  var $addProject = $('[data-add-project]'),
+    $modalTarp = $('.modal-tarp'),
+    $projectModal = $('.add-project'),
+    $cancelProject = $('[data-cancel-project]'),
+    fadeSpeed = 200;
 
+  var closeModal = function() {
+    $modalTarp.fadeOut(fadeSpeed);
+    $projectModal.removeClass('modal-open');
+  }
 
-      // Fade out button, fade in add project instructions
-      $addProject.on('click', function(e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $modalTarp.fadeIn(fadeSpeed);
-        $projectModal.addClass('modal-open');
-      });
+  // Fade out button, fade in add project instructions
+  $addProject.on('click', function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $modalTarp.fadeIn(fadeSpeed);
+    $projectModal.addClass('modal-open');
+  });
 
-      // Fade out project instruction, fade in button
-      $cancelProject.on('click', function(e) {
-        e.preventDefault();
+  // Fade out project instruction, fade in button
+  $cancelProject.on('click', function(e) {
+    e.preventDefault();
+    closeModal();
+  });
+
+  $('body').on('click', function(e) {
+    if ($modalTarp.is(':visible')) {
+      if ($(e.target).is('.modal-tarp')) {
         closeModal();
-      });
+      }
+    }
+  });
 
-      $('body').on('click', function(e) {
-        if ($modalTarp.is(':visible')) {
-          if ($(e.target).is('.modal-tarp')) {
-            closeModal();
-          }
-        }
-      });
-
-      // Esc functions
-      $(document).keyup(function(e) {
-        if ($modalTarp.is(':visible')) {
-          if (e.keyCode == 27) { // esc
-            $modalTarp.fadeOut(fadeSpeed);
-            $projectModal.fadeOut(fadeSpeed);
-          }
-        }
-      });
+  // Esc functions
+  $(document).keyup(function(e) {
+    if ($modalTarp.is(':visible')) {
+      if (e.keyCode == 27) { // esc
+        $modalTarp.fadeOut(fadeSpeed);
+        $projectModal.fadeOut(fadeSpeed);
+      }
+    }
+  });
 
 });
