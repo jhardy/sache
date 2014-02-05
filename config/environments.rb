057c240a828eb11741f26331a9d7d9a2b3a72dea
Jared Hardy <jared@jaredhardy.com>
2014-02-05 12:31:34 -0800

    more heroku stuff

--- config/environments.rb
+++ config/environments.rb
@@ -3,7 +3,7 @@
 # => postgres://{user}:{password}@{host}:{port}/path
 #This is automatically configured on Heroku, you only need to worry if you also
 #want to run your app locally
-configure :production, :development do
+configure :development do
         db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/sachein_dev')
 
         ActiveRecord::Base.establish_connection(
@@ -14,4 +14,9 @@ configure :production, :development do
                         :database => db.path[1..-1],
                         :encoding => 'utf8'
         )
+end
+
+configure :production do 
+	require 'active_record'
+	ActiveRecord::Base.establish_connection(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 end
