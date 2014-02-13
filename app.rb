9643b171383cc1a25a595deb4153527dd274acc8
Jared Hardy <jared@jaredhardy.com>
2014-02-12 21:10:26 -0800

    use only ssh url for repos to help avoid duplicates

--- app.rb
+++ app.rb
@@ -94,8 +94,9 @@ end
 
 post '/extensions' do
     unless params[:project_url].empty?
+
         begin
-            project_url =  %r{github\.com[:/](.+?)/(.+?)(?:\.git|)$}i.match(params[:project_url])
+            project_url =  %r{github\.com[:/](.+?)/(.+?)(?:\.git)$}i.match(params[:project_url])
             username = project_url[1]
             reponame = project_url[2]
         rescue NoMethodError => e
