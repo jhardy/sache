9a3601e3f99df8574488ba1dd5b0c2b0a4db9c35
Jared Hardy <jared@jaredhardy.com>
2014-03-29 07:23:26 -0700

    add rake backup db task

--- rakefile.rb
+++ rakefile.rb
@@ -7,3 +7,9 @@ task :update_extensions do
     app = Sinatra::Application.new
     app.helpers.upldate_all
 end
+
+
+desc "Backup Production Database"
+task :backup_db do
+	sh "heroku pgbackups:capture"
+end
